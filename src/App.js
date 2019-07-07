2ec62f7a7f2b53bf3e7420b6db0b237ba72cf38c
Sebastian Kurpiel <sebbykurp@gmail.com>
2019-07-07 11:24:05 +0900

    Added prettier

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
-import React, {useState} from 'react';
-import {Helmet} from "react-helmet";
-import ReactMapGL , {Marker} from "react-map-gl"
-import styled from '@emotion/styled'
-import './App.css';
+import React, { useState } from "react"
+import { Helmet } from "react-helmet"
+import ReactMapGL, { Marker } from "react-map-gl"
+import styled from "@emotion/styled"
+import "./App.css"
 
 const Tokyo = {
-  coords: [35.685360,139.753372]
+  coords: [35.68536, 139.753372]
 }
 
-const MapMarkerOuterCircle = styled.button `
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width:20px;
-    height:20px;
-    border-radius: 10px;
-    background-color: #DD4B39;
-    padding:0 ;
-    cursor: pointer;
-    border: none;
-    z-index: 0;
+const MapMarkerOuterCircle = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+  background-color: #dd4b39;
+  padding: 0;
+  cursor: pointer;
+  border: none;
+  z-index: 0;
 `
 
-const MapMarkerInnerCircle = styled.div `
-    width:10px;
-    height:10px;
-    border-radius: 5px;
-    background-color: #2a2a2a;
+const MapMarkerInnerCircle = styled.div`
+  width: 10px;
+  height: 10px;
+  border-radius: 5px;
+  background-color: #2a2a2a;
 `
 
-const MapMarker = ({latitude,longitude, onClick}) => {
-
+const MapMarker = ({ latitude, longitude, onClick }) => {
   return (
-      <Marker latitude={latitude} longitude={longitude}>
-        <MapMarkerOuterCircle onClick={() => onClick({
-          width: window.innerWidth,
-          height: window.innerHeight,zoom:10,
-          latitude,
-          longitude})}>
-          <MapMarkerInnerCircle />
-        </MapMarkerOuterCircle>
-      </Marker>
+    <Marker latitude={latitude} longitude={longitude}>
+      <MapMarkerOuterCircle
+        onClick={() =>
+          onClick({
+            width: window.innerWidth,
+            height: window.innerHeight,
+            zoom: 10,
+            latitude,
+            longitude
+          })
+        }>
+        <MapMarkerInnerCircle />
+      </MapMarkerOuterCircle>
+    </Marker>
   )
 }
 function App() {
@@ -60,14 +64,14 @@ function App() {
         <title>WTFIB</title>
       </Helmet>
       <ReactMapGL
-          {...viewport}
-          mapStyle={"mapbox://styles/sebastiankurp/cjxsbmb5f79rd1cp6511l5aii"}
-          mapboxApiAccessToken="pk.eyJ1Ijoic2ViYXN0aWFua3VycCIsImEiOiJjandwZWZ1emkxOHR1NDhwOG1lM2pmeHVmIn0.fHuAftP7b6uRy1UfWieSPQ"
-          onViewportChange={viewport => setViewport(viewport)}>
-        <MapMarker latitude={Tokyo.coords[0]} longitude={Tokyo.coords[1]} onClick={setViewport}/>
+        {...viewport}
+        mapStyle={"mapbox://styles/sebastiankurp/cjxsbmb5f79rd1cp6511l5aii"}
+        mapboxApiAccessToken="pk.eyJ1Ijoic2ViYXN0aWFua3VycCIsImEiOiJjandwZWZ1emkxOHR1NDhwOG1lM2pmeHVmIn0.fHuAftP7b6uRy1UfWieSPQ"
+        onViewportChange={viewport => setViewport(viewport)}>
+        <MapMarker latitude={Tokyo.coords[0]} longitude={Tokyo.coords[1]} onClick={setViewport} />
       </ReactMapGL>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
